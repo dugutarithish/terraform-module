d3d2bd0e793555ab9d4f04d702b090c3392c809d
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 15:24:48 +0530

    Create variable.tf

---
+++ module/aws/variable.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+  description = "THE AMI ID"
+  type        = string  
+}
+
+variable "instance_type_value" {
+  description = "THE INSTANCE TYPE"
+  type        = string  
+}
+
+variable "subnet_id_value" {
+  description = "THE SUBNET ID"
+  type        = string  
+}
